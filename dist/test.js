447eaebbf55950363cb060bad05bed401d74c132
유석규 <ysk870512@gmail.com>
2019-04-11 11:05:20 +0900

    크라운 앤 앵커 게임

--- dist/test.js
+++ dist/test.js
@@ -21,10 +21,6 @@ function say({
   console.log(`${subject} ${verb} ${object}`);
 }
 
-;
-
 for (let s of sentences) {
   say(s);
 }
-
-;
--- es6/test.js
+++ es6/test.js
@@ -10,7 +10,7 @@ function say({ subject, verb, object }) {
     // 아래 사용한 것은 따옴표가 아니라 백틱(`), 즉
     // 키보드의 탭 키 바로 위 에 있는 문자입니다,
     console.log(`${subject} ${verb} ${object}`);
-};
+}
 for (let s of sentences) {
     say(s);
-};
+}
